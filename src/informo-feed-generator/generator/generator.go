4d965eaa6137fa9d94c0ffb57538bc4594f4fcbb
Vabd <vabd@anon.acme>
2018-03-21 23:13:01 +0100

    Add license + log

--- src/informo-feed-generator/generator/generator.go
+++ src/informo-feed-generator/generator/generator.go
@@ -1,3 +1,18 @@
+// Copyright 2018 Informo core team <core@informo.network>
+//
+// Licensed under the GNU Affero General Public License, Version 3.0
+// (the "License"); you may not use this file except in compliance with the
+// License.
+// You may obtain a copy of the License at
+//
+//     https://www.gnu.org/licenses/agpl-3.0.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package generator
 
 import (
@@ -37,6 +52,7 @@ func NewGenerator(db *database.Database, cfg *config.FeedsConfig) *Generator {
 func (g *Generator) SetupAndServe() error {
 	g.setup()
 	listenAddr := fmt.Sprintf("%s:%d", g.cfg.Interface, g.cfg.Port)
+	logrus.Info("Starting Web server, listening on " + listenAddr)
 	return http.ListenAndServe(listenAddr, g.mux)
 }
 
